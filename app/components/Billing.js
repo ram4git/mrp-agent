ad6bb4ce6073f89f26be86f8e046bfea6533bcb7
Ram <ram@cliqr.com>
2017-08-15 02:47:22 +0530

    fixed placeholders

--- app/components/Billing.js
+++ app/components/Billing.js
@@ -20,7 +20,7 @@ class Billing extends Component {
             <Form.Select label='Regionality' options={ getMasters('regions')} placeholder='AP or Non AP?' width={4} />
             <Form.Select label='Lorry Type' options={ getMasters('lorryTypes') } placeholder='How many wheels?' width={4} />
             <Form.Select label='Product Type' options={ getMasters('products') } placeholder='Rice or Paddy?' width={4} />
-            <Form.Select label='Action' options={ getMasters('actions') } placeholder='loading or unloading?' width={4} />
+            <Form.Select label='Action' options={ getMasters('actions') } placeholder='Loading or Unloading?' width={4} />
           </Form.Group>
           <div className="activities">
             { this.renderActivities() }
@@ -41,10 +41,10 @@ class Billing extends Component {
     for( let i = 0; i < this.state.activityRows; i++) {
       activities.push(
         <Form.Group key={i}>
-          <Form.Input label='W.S.NO' placeholder='W.S.NO' width={3} />
-          <Form.Input label='Lorry NO' placeholder='Lorry NO' width={4} />
-          <Form.Select label='Jattu' options={ getMasters('jattus') } placeholder='Jattu' width={4} />
-          <Form.Input label='Weight in Tons' placeholder='Weight' width={3} />
+          <Form.Input label='W.S.NO' placeholder='#####' width={3} />
+          <Form.Input label='Lorry NO' placeholder='AP12CD1234' width={4} />
+          <Form.Select label='Jattu' options={ getMasters('jattus') } placeholder='Jattu Name' width={4} />
+          <Form.Input label='Weight in Tons' placeholder='00.00' width={3} />
           { (i + 1) === this.state.activityRows ?
             <Form.Button label="New Row" content='+' width={2} onClick={this.addActivityRow.bind(this)} /> :
             <Form.Button label="Delete Row" content='-' width={2} onClick={this.removeActivityRow.bind(this)} /> }
